041c0687abb7ff12f6c0876a979a80be61271452
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2021-01-04 12:33:54 -0300

    Files of my website - PREVGOCIOS
    I'm still learning to develop right

---
+++ script.js
@@ -0,0 +1,25 @@
+function mascara(i,t){
+
+    var v = i.value;
+
+    if(isNaN(v[v.length-1])){
+        i.value = v.substring(0, v.length-1);
+        return;
+    }
+  
+    if (t == "cpf"){
+        i.setAttribute("maxlength", "14");
+        if (v.length == 3 || v.length == 7) i.value += ".";
+        if (v.length == 11) i.value += "-";
+    }
+    if(t == "tel"){
+        if(v[0] == 9){
+           i.setAttribute("maxlength", "10");
+           if (v.length == 5) i.value += "-";
+        }else{
+           i.setAttribute("maxlength", "9");
+           if (v.length == 4) i.value += "-";
+        }
+    }
+
+}
